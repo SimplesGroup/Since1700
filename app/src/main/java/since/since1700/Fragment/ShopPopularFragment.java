af4dc0145bca3e2d005fc0496792308b1a931fb4
sandhiya <sandhiya@simples.in>
2017-09-22 11:38:30 +0530

    Fragments update

---
+++ app/src/main/java/since/since1700/Fragment/ShopPopularFragment.java
@@ -0,0 +1,28 @@
+package since.since1700.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import since.since1700.R;
+
+/**
+ * Created by Sandhiya on 9/21/2017.
+ */
+
+public class ShopPopularFragment extends Fragment {
+    @Nullable
+    public static ShopPopularFragment newInstance() {
+        ShopPopularFragment fragment = new ShopPopularFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.feed_fragment, container, false);
+        return view;
+    }
+}
